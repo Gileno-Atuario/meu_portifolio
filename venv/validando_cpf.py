ba5aa1812f062459e8dc62bcc4cfb0821e6a4451
note_hp <gileno.atuario@gmail.com>
2022-12-14 08:01:04 -0300

    Aplicando expressão regular ao código.

--- venv/validando_cpf.py
+++ venv/validando_cpf.py
@@ -1,23 +1,22 @@
+import re
 while True:
-    valor = input('Digite o seu CPF (formato: 00000000000): ').strip()
-    elementos = []
-    for elemento in valor:
-        elementos.append(elemento)
-    if len(set(elementos)) == 1:
-        print('Inválido! Valor não pode conter todos números iguais.')
+    entrada = input('Digite o seu CPF: ')
+    cpf_usuario = re.sub(r'[^0-9]', '', entrada)
+    elementos_cpf_usuario = []
+    for elemento in cpf_usuario:
+        elementos_cpf_usuario.append(elemento)
+    if len(set(elementos_cpf_usuario)) == 1:
+        print('Inválido! CPF não pode conter todos números iguais.')
         continue
-    if len(valor) != 11:
-        print('Inválido! Valor não contém 11 digitos.')
-        continue
-    if not valor.isnumeric():
-        print('Inválido! Valor deve conter somente números.')
+    if len(cpf_usuario) != 11:
+        print('Inválido! CPF não contém 11 digitos.')
         continue
     i = 10
     soma = 0
     digito_x_regressao = []
     validacao = []
     validacao_strig =[]
-    for j, l in enumerate(valor, start=1):
+    for j, l in enumerate(cpf_usuario, start=1):
         if j < 10:
             validacao_strig.append(l)
             digito = int(l)
@@ -37,7 +36,7 @@ while True:
         validacao_strig.append(str(maior_q_dez))
     m = 11
     soma_1 = 0
-    for n, o in enumerate(valor, start=1):
+    for n, o in enumerate(cpf_usuario, start=1):
         if n < 11:
             digito_1 = int(o)
             multiplicacao_1 = m * digito_1
@@ -52,8 +51,8 @@ while True:
         maior_q_dez_v2 = 0
         validacao.append(maior_q_dez_v2)
         validacao_strig.append(str(maior_q_dez_v2))
-    cpf = f'{valor[:3]}.{valor[3:6]}.{valor[6:9]}-{valor[9:]}'
-    if elementos == validacao_strig:
+    cpf = f'{cpf_usuario[:3]}.{cpf_usuario[3:6]}.{cpf_usuario[6:9]}-{cpf_usuario[9:]}'
+    if elementos_cpf_usuario == validacao_strig:
         print(f'O CPF {cpf} É VÁLIDO!')
     else:
         print(f'O CPF {cpf} É INVÁLIDO!')
